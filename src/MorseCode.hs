99efb8de19db08af42e53c2f2571450fcc80cbab
Edmondo Pentangelo <edmondo@gmail.com>
2021-12-20 18:32:16 +0000

    Add basic components

--- src/MorseCode.hs
+++ src/MorseCode.hs
@@ -5,12 +5,15 @@ module Main where
 import Control.Monad.Trans ()
 import System.Console.Haskeline ()
 import Data.Maybe ( fromMaybe )
+import Control.Monad.IO.Class ( MonadIO(liftIO) )
 import System.Environment (lookupEnv)
 import Web.Scotty         (ScottyM, scotty)
-import Web.Scotty.Trans ( body, json, post, middleware )
+import Web.Scotty.Trans ( body, json, post, middleware, setHeader )
 import Network.Wai.Middleware.RequestLogger ( logStdout )
 import Data.ByteString.Lazy.Char8 as Char8 ( unpack )
-import Data.Text (pack)
+import Data.Text as Text
+import Data.Text.Lazy as Lazy
+import qualified Data.UUID.V1 as U1
 
 main :: IO ()
 main = do
@@ -25,5 +28,10 @@ route = do
     post "/echo" $ do
          input <- body
          let ret = Char8.unpack input
-         json $ pack ret
+         (Just k) <- liftIO U1.nextUUID 
+         setHeader "Ce-Id" (Lazy.pack $ show k)
+         setHeader "Ce-Specversion" "1.0"
+         setHeader "Ce-Source" "/morse-code"
+         setHeader "Ce-Type" "morseCode"
+         json $ Text.pack ret
 
