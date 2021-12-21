c8f22388477e428773b0999e82eafe02c92e91dc
Edmondo Pentangelo <edmondo@gmail.com>
2021-12-21 14:00:24 +0000

    Save input to Redis

--- src/MorseCode.hs
+++ src/MorseCode.hs
@@ -10,10 +10,11 @@ import System.Environment (lookupEnv)
 import Web.Scotty         (ScottyM, scotty)
 import Web.Scotty.Trans ( body, text, post, middleware, setHeader )
 import Network.Wai.Middleware.RequestLogger ( logStdout )
-import Data.ByteString.Lazy.Char8 as Char8 ( unpack )
+import Data.ByteString.Lazy.Char8 as Char8Lazy ( unpack )
+import Data.ByteString.Char8 as Char8 ( pack )
 import Data.Text.Lazy as Lazy ( pack )
 import qualified Data.UUID.V1 as U1
-import Database.Redis (Connection, ConnectInfo, connect, connectHost, defaultConnectInfo, runRedis, set)
+import Database.Redis (Connection, ConnectInfo, connect, connectHost, defaultConnectInfo, runRedis, set, get)
 
 main :: IO ()
 main = do
@@ -31,9 +32,10 @@ route redisCon = do
     middleware logStdout
     post "/echo" $ do
          input <- body
-         let ret = Char8.unpack input
+         let ret = Char8Lazy.unpack input
          _ <- liftIO $ runRedis redisCon $ do
-                         set "hello" "hello"
+                         Right (Just x) <- get (Char8.pack ret)
+                         set "hello" x
          (Just k) <- liftIO U1.nextUUID 
          setHeader "Ce-Id" (Lazy.pack $ show k)
          setHeader "Ce-Specversion" "1.0"
