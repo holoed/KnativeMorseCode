ddb4172846237a3fb3a830854a96315467a54244
Edmondo Pentangelo <edmondo@gmail.com>
2021-12-21 15:05:09 +0000

    Experiments

--- src/MorseCode.hs
+++ src/MorseCode.hs
@@ -11,10 +11,10 @@ import Web.Scotty         (ScottyM, scotty)
 import Web.Scotty.Trans ( body, text, post, middleware, setHeader )
 import Network.Wai.Middleware.RequestLogger ( logStdout )
 import Data.ByteString.Lazy.Char8 as Char8Lazy ( unpack )
-import Data.ByteString.Char8 as Char8 ( pack )
+import Data.ByteString.Char8 as Char8 ( pack, unpack )
 import Data.Text.Lazy as Lazy ( pack )
 import qualified Data.UUID.V1 as U1
-import Database.Redis (Connection, ConnectInfo, connect, connectHost, defaultConnectInfo, runRedis, set, get)
+import Database.Redis (Connection, ConnectInfo, connect, connectHost, defaultConnectInfo, runRedis, set, get, Reply (Integer))
 
 main :: IO ()
 main = do
@@ -22,7 +22,7 @@ main = do
   pStr <- fromMaybe "8080" <$> lookupEnv "PORT"
   redisCon <- connect connectionInfo
   let p = read pStr :: Int
-  scotty p (route redisCon) 
+  scotty p (route redisCon)
 
 connectionInfo :: ConnectInfo
 connectionInfo = defaultConnectInfo { connectHost  = "redis-master" }
@@ -36,9 +36,10 @@ route redisCon = do
          _ <- liftIO $ runRedis redisCon $ do
                          v <- get (Char8.pack ret)
                          case v of
-                          Right (Just x) -> set "hello" x
+                          Right (Just x) -> set "hello" (Char8.pack (ret ++ Char8.unpack x))
+                          Right Nothing -> set "hello" (Char8.pack ret)
                           other -> set "hello" (Char8.pack (show other))
-         (Just k) <- liftIO U1.nextUUID 
+         (Just k) <- liftIO U1.nextUUID
          setHeader "Ce-Id" (Lazy.pack $ show k)
          setHeader "Ce-Specversion" "1.0"
          setHeader "Ce-Source" "/morse-code"
